88937f96e80eb8204650c5fad7c336c3cf9d36ef
Dragos Petrescu <petrescudragos19@stud.ase.ro>
2022-03-07 11:15:37 +0200

    al treilea commit

---
+++ Seminar2CTS/src/ase/cts/clase/readers/StudentiReader.java
@@ -0,0 +1,34 @@
+package ase.cts.clase.readers;
+
+import ase.cts.clase.Aplicant;
+import ase.cts.clase.Elev;
+import ase.cts.clase.Student;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class StudentiReader extends IReader{
+
+    @Override
+    public List<Aplicant> readAplicanti(String file) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(file));
+        input.useDelimiter(",|\n");
+        List<Aplicant> studenti = new ArrayList<Aplicant>();
+
+        while (input.hasNext()) {
+            Student student = new Student();
+            super.citireAplicant(input,student);
+            int an_studii = input.nextInt();
+            String facultate = (input.next()).toString();
+            student.setFacultate(facultate);
+            student.setAn_studii(an_studii);
+            studenti.add(student);
+        }
+        input.close();
+        return studenti;
+    }
+
+}
--- src/Sem1.java
+++
@@ -1,3 +0,0 @@
-public class Sem1 {
-
-}
